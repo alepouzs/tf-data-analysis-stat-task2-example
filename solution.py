08b056ba3988993d8a604fafb588ba58908e67e1
Yulia Izmailova <104653247+alepouzs@users.noreply.github.com>
2023-04-08 19:39:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,4 +7,4 @@ from scipy.stats import norm
 chat_id = 487382438 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    return np.max(x), (np.max(x - 0.053) / ((1 - p) ** (1 / len(x))) + 0.053
+    return np.max(x), np.max(x - 0.053) / ((1 - p) ** (1 / len(x))) + 0.053
